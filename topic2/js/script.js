4a8c5c3e30ed99c48a2c481bfb82b317403654a2
Leandro Bisceglie <leam.90@gmail.com>
2018-02-28 13:39:04 -0300

    class prac

---
+++ topic2/js/script.js
@@ -0,0 +1,20 @@
+class Movie {
+    constructor(name, year, duration) {
+        this.name = name;
+        this.year = year;
+        this.duration = duration;
+    }
+    play() {
+        console.log("Play..");
+    }
+
+    pause() {
+        console.log("Pause..");
+    }
+
+    resume() {
+        console.log("Resume..");
+    }
+}
+var movie = new Movie('pepe',1999,"50:00");
+console.log(movie)
